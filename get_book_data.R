41a1668246584c0f2fe57333cf376e5828d23e71
Vitaly Druker <vitaly@westendstats.com>
2021-09-02 14:13:11 -0400

    finished materials for class 2

---
+++ get_book_data.R
@@ -0,0 +1,13 @@
+if(!dir.exists(here::here('ma_book_data'))) {
+  library(curl)
+  url <- "ftp://ftp.wiley.com/public/sci_tech_med/multivariate_analysis_3e/multivariate_analysis%20-%203rd%20Ed.zip"
+
+  temp_file <- tempfile(fileext = ".zip")
+
+  curl_download(url, destfile = temp_file)
+  unzip(temp_file, exdir = here::here())
+  file.rename(here::here('multivariate_analysis - 3rd Ed/'),
+              here::here('ma_book_data'))
+
+}
+
